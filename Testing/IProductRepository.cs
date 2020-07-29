487ec861f26c3bbbfb5075363023a3132b292994
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-07-29 12:10:41 -0500

    Commit to add functions to insert, edit, and delete products from the bestbuy.products table

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product getProduct(int id);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
